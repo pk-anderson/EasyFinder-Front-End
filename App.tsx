0822fdec1955e2910fb980b12367451d06e9af21
patrick <patrick.estagiario@ms.touk.io>
2023-05-29 19:10:06 -0300

    logo

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Button from './src/components/Button';
 
-const logoImage = require('./src/assets/logo.jpg');
+const logoImage = require('./src/assets/logo.png');
 
 export default function App() {
   const handleLoginPress = () => {
