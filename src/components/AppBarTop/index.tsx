88773bcc1a6047b02f2f767ff6df405092ac9ae3
patrick <patrick.estagiario@ms.touk.io>
2023-06-07 19:00:12 -0300

    add side menu button

--- src/components/AppBarTop/index.tsx
+++ src/components/AppBarTop/index.tsx
@@ -1,12 +1,61 @@
-import { Image,Text, View } from "react-native";
-import menuImg from '../../assets/menu-aberto.png'
-import { styles } from "./styles";
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+import menuImg from '../../assets/menu-aberto.png';
+import { deviceDimensions } from "../../global/dimesion";
+import { styles } from './style';
 
-export default function AppTopBar(){
-return(
+type AppTopBarProps = {
+  // Adicione as propriedades necessárias
+};
+
+const AppTopBar: React.FC<AppTopBarProps> = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleEditProfile = () => {
+    // Lógica para realizar o logout
+    console.log('Edit Profile');
+  };
+
+  const handleLogout = () => {
+    // Lógica para realizar o logout
+    console.log('Logout');
+  };
+
+  return (
     <View style={styles.containerTop}>
-    <Image source={menuImg} style={styles.menu}/>
-    <Text style={styles.logo}>LOGO</Text>
-</View>
-)
+      <TouchableOpacity style={styles.menuButton} onPress={handleMenuToggle}>
+        <Image source={menuImg} style={styles.menuIcon} />
+      </TouchableOpacity>
+
+      {/* Menu Lateral */}
+      <Modal visible={isMenuOpen} animationType="slide" transparent>
+        <View style={styles.menuContainer}>
+          <View style={styles.userInfoContainer}>
+            <Image source={require('../../assets/avatar.jpg')} style={styles.userPhoto} />
+          <Text style={styles.userName}>{ "Nome do usuário" }</Text>
+          </View>
+
+          <TouchableOpacity style={styles.menuItem} onPress={handleMenuToggle}>
+            <Text style={styles.menuItemText}>Voltar ao Início</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.menuItem} onPress={handleEditProfile}>
+            <Text style={styles.menuItemText}>Visualizar Perfil</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.menuItem} onPress={handleLogout}>
+            <Text style={styles.menuItemText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      
+      <Text style={styles.logo}>LOGO</Text>
+    </View>
+  );
 }
+
+export default AppTopBar;
---
+++ src/components/AppBarTop/style.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+import { deviceDimensions } from "../../global/dimesion";
+
+export const styles = StyleSheet.create({
+    containerTop: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingTop: 10,
+      },
+      menuButton: {
+        padding: 10,
+      },
+      menuIcon: {
+        width: 24,
+        height: 24,
+        tintColor: '#fff',
+        marginRight: 30,
+      },
+      menuContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        width: deviceDimensions().width * 0.7,
+      },
+      userInfoContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20,
+      },
+      userPhoto: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+      },
+      userName: {
+        fontSize: 16,
+        fontWeight: 'bold',
+      },
+      menuItem: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+      },
+      menuItemText: {
+        fontSize: 16,
+      },
+      logo: {
+        color: 'white',
+        fontSize: 30,
+      },
+})
