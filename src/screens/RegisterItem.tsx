49d76c1cfedad449676b5aafb1f2b9c0f921181d
Marcos Lorhan <marcoslorhanbs@gmail.com>
2023-07-03 17:17:48 -0300

    DateTimePicker Update

--- src/screens/RegisterItem.tsx
+++ src/screens/RegisterItem.tsx
@@ -4,6 +4,7 @@ import BaseScreen from "./BaseScreen";
 import ItemTopBar from '../components/ItemPage';
 import ImageUploadField from '../components/UploadImage';
 import MapView, {Marker, LatLng} from 'react-native-maps';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import Button from '../components/Button';
 
 
@@ -17,11 +18,39 @@ const RegisterItemScreen: React.FC<Props> = () => {
   const [productDescription, setProductDescription] = useState('');
   const [productLocalization, setProductLocalization] = useState('');
   const [markerCoords, setMarkerCoords] = useState<LatLng | undefined>(undefined);
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const BRDay = date.getDay.toString()
+  const BRMonth = (date.getMonth() +1).toString()
+  const BRYear = date.getFullYear.toString()
+  const BRFullDate = (BRDay + BRMonth + BRYear)
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate)
+    console.log(date);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+
 
   const handleMapPress = (event: any) => {
     const { coordinate } = event.nativeEvent;
     setMarkerCoords(coordinate);
   };
+
+  
 
   return (
     <BaseScreen
@@ -44,8 +73,21 @@ const RegisterItemScreen: React.FC<Props> = () => {
             onChangeText={text => setProductDescription(text)}
             multiline
           />
+          <Text style={styles.text}>DATA QUE O ENCONTROU</Text>
+          <Button onPress={showDatepicker} text="Informar Data" />
+          {/* date.toLocaleDateString('pt-br') */}
+          <Text>selecionado: {date.toLocaleDateString('pt-br')}</Text>
+          {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              is24Hour={false}
+              onChange={onChange}
+              locale='pt-BR'
+            />
+          )}
           <Text style={styles.text}>{'\n'}LOCAL QUE O VIU PELA ULTIMA VEZ{'\n'}</Text>
-          <View style={{height: 220}}>
+          <View style={{height: 200}}>
             <MapView
               onPress={handleMapPress}
               style={{ flex: 1}}
@@ -95,7 +137,7 @@ const styles = StyleSheet.create({
     borderWidth: 1.5,
     borderColor: '#6F6F6F',
     borderRadius: 4,
-    padding: 8,
+    padding: 5,
     marginBottom: 16,
   },
   saveBtn: {
@@ -109,6 +151,10 @@ const styles = StyleSheet.create({
     left: 100,
     color: 'white',
     fontSize: 30
+  },
+  datePicker: {
+    width: 200,
+    marginBottom: 20
   }
 });
 
